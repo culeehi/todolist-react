67d37987fa2d1ae633aba9207777328955a9ae98
thanhdong <thanhdongtran77@gmail.com>
2023-05-04 01:52:27 +0700

    handleAddSv

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+// import { useState, useEffect } from 'react';
 
-const Input = (props) => {
-   const { setData, classStyle, lable } = props;
-   return (
-      <>
-         <label>{lable}</label>
-         <input
-            type="text"
-            className={classStyle}
-            onChange={(e) => {
-               setData(e.target.value);
-            }}
-         />
-      </>
-   );
-};
+// const UseCaseFetchApi = (props) => {
+//    const [bio, setBio] = useState({});
 
-export default Input;
+//    useEffect(() => {
+//       const fetchData = async () => {
+//          const response = await fetch('https://6440e462792fe886a8986bf7.mockapi.io/api/v1/students');
+//          const data = await response.json();
+//          console.log(data);
+//          setBio(data);
+//       };
+//       fetchData();
+//    }, []);
+
+//    return (
+//       <>
+//          <hr />
+//          <h2>useEffect use case</h2>
+//          <h3>Running once on mount: fetch API data</h3>
+//          <p>bio:</p>
+//          {/* <pre>{JSON.stringify(bio, null, '\t')}</pre> */}
+//          {setBio}
+//       </>
+//    );
+// };
+
+// export default UseCaseFetchApi;
--- src/components/TodoListUser/ListUser.jsx
+++ src/components/TodoListUser/ListUser.jsx
@@ -4,9 +4,9 @@ import './listuser.css';
 const ListUser = () => {
    const [user, setUser] = useState([]);
 
-   const [name, setName] = useState();
-   const [code, setCode] = useState();
-   const [date, setDate] = useState();
+   const [name, setName] = useState('');
+   const [code, setCode] = useState('');
+   const [date, setDate] = useState('');
 
    const [errors, setErrors] = useState({
       name: '',
@@ -14,15 +14,85 @@ const ListUser = () => {
       date: '',
    });
 
-   const [formData, setFormData] = useState({
-      name: '',
-      code: '',
-      date: '',
-   });
+   // const [formData, setFormData] = useState({
+   //    name: '',
+   //    code: '',
+   //    date: '',
+   // });
 
-   const handleInputChange = (event) => {
-      const { name, value } = event.target;
-      setFormData({ ...formData, [name]: value });
+   const handleAddSv = () => {
+      fetch('https://6440e462792fe886a8986bf7.mockapi.io/api/v1/students', {
+         method: 'POST',
+         headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+         },
+         body: JSON.stringify({
+            code: code,
+            date: date,
+            name: name,
+            completed: false,
+         }),
+      })
+         .then((response) => {
+            if (response.ok) {
+               return response.json();
+            }
+         })
+         .then((data) => {
+            setUser([...user, data]);
+
+            setName('');
+            setCode('');
+            setDate('');
+         })
+         .catch((error) => console.error('Error adding item:', error));
+   };
+
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      let formIsValid = false;
+
+      const newErrors = {};
+
+      if (!name.name) {
+         newErrors.name = 'Không được để trống';
+         formIsValid = false;
+      } else if (name.name.length < 2) {
+         newErrors.name = 'Tên của sinh viên phải lớn hơn 2 kí tự';
+         formIsValid = false;
+      } else if (name.name.length > 30) {
+         newErrors.name = 'Tên của sinh viên phải bé hơn 30 kí tự';
+         formIsValid = false;
+      }
+
+      if (!code.code) {
+         newErrors.code = 'Không được để trống';
+         formIsValid = false;
+      } else if (code.code.length < 3) {
+         newErrors.code = 'Mã sinh viên phải có 4 kí tự';
+         formIsValid = false;
+      } else if (code.code.length > 5) {
+         newErrors.code = 'Mã sinh viên phải có 4 kí tự';
+         formIsValid = false;
+      }
+
+      if (!date.date) {
+         newErrors.date = 'Không được để trống';
+         formIsValid = false;
+      } else if (date.date.length < 3) {
+         newErrors.date = 'Năm sinh phải có 4 chứ số';
+         formIsValid = false;
+      } else if (date.date.length > 5) {
+         newErrors.date = 'Năm sinh phải có 4 chứ số';
+         formIsValid = false;
+      }
+
+      setErrors(newErrors);
+
+      console.log('formIsValid:', formIsValid);
+      if (formIsValid) {
+         handleAddSv();
+      }
    };
 
    const handleDelete = (userId) => {
@@ -36,88 +106,7 @@ const ListUser = () => {
          .catch((error) => console.error('Error deleting item:', error));
    };
 
-   const handleSubmit = (event) => {
-      event.preventDefault();
-      let formIsValid = true;
-
-      const newErrors = {};
-
-      if (!formData.name) {
-         newErrors.name = 'Không được để trống';
-         formIsValid = false;
-      } else if (formData.name.length < 2) {
-         newErrors.name = 'Tên của sinh viên phải lớn hơn 2 kí tự';
-         formIsValid = false;
-      } else if (formData.name.length > 30) {
-         newErrors.name = 'Tên của sinh viên phải bé hơn 30 kí tự';
-         formIsValid = false;
-      }
-
-      if (!formData.code) {
-         newErrors.code = 'Không được để trống';
-         formIsValid = false;
-      } else if (formData.code.length < 3) {
-         newErrors.code = 'Mã sinh viên phải có 4 kí tự';
-         formIsValid = false;
-      } else if (formData.code.length > 5) {
-         newErrors.code = 'Mã sinh viên phải có 4 kí tự';
-         formIsValid = false;
-      }
-
-      if (!formData.date) {
-         newErrors.date = 'Không được để trống';
-         formIsValid = false;
-      } else if (formData.date.length < 3) {
-         newErrors.date = 'Năm sinh phải có 4 chứ số';
-         formIsValid = false;
-      } else if (formData.date.length > 5) {
-         newErrors.date = 'Năm sinh phải có 4 chứ số';
-         formIsValid = false;
-      }
-
-      setErrors(newErrors);
-
-      if (formIsValid) {
-         console.log('Form data:', formData);
-      }
-   };
-
-   if (!handleSubmit) {
-      console.log('nooo');
-   } else {
-      console.log('hihi');
-   }
-
    const handleEdit = () => {};
-
-   // const handleAddSv = (e) => {
-   //    e.preventDefault();
-   //    fetch('https://6440e462792fe886a8986bf7.mockapi.io/api/v1/students', {
-   //       method: 'POST',
-   //       headers: {
-   //          'Content-type': 'application/json; charset=UTF-8',
-   //       },
-   //       body: JSON.stringify({
-   //          code: code,
-   //          date: date,
-   //          name: name,
-   //          completed: false,
-   //       }),
-   //    })
-   //       .then((response) => {
-   //          if (response.ok) {
-   //             return response.json();
-   //          }
-   //       })
-   //       .then((data) => {
-   //          setUser([...user, data]);
-
-   //          setName('');
-   //          setCode('');
-   //          setDate('');
-   //       })
-   //       .catch((error) => console.error('Error adding item:', error));
-   // };
 
    useEffect(() => {
       fetch('https://6440e462792fe886a8986bf7.mockapi.io/api/v1/students')
@@ -143,15 +132,39 @@ const ListUser = () => {
             </button>
             <div className="input-student">
                <label htmlFor="code">Mã sinh viên</label>
-               <input type="text" id="code" name="code" value={formData.code} onChange={handleInputChange} />
+               <input
+                  type="text"
+                  id="code"
+                  name="code"
+                  value={code}
+                  onChange={(e) => {
+                     setCode(e.target.value);
+                  }}
+               />
                {errors.code && <p className="error-validate">{errors.code}</p>}
 
                <label htmlFor="name">Tên sinh viên</label>
-               <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} />
+               <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={(e) => {
+                     setName(e.target.value);
+                  }}
+               />
                {errors.name && <p className="error-validate">{errors.name}</p>}
 
                <label htmlFor="date">Năm sinh </label>
-               <input type="text" id="date" name="date" value={formData.date} onChange={handleInputChange} />
+               <input
+                  type="text"
+                  id="date"
+                  name="date"
+                  value={date}
+                  onChange={(e) => {
+                     setDate(e.target.value);
+                  }}
+               />
                {errors.date && <p className="error-validate">{errors.date}</p>}
             </div>
          </form>
